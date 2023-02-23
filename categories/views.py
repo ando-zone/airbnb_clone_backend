c3b95b5e46d0df32a594fea6f42fe7fd08172744
ando-zone <dev.dohyeonan@gmail.com>
2023-02-23 18:22:50 +0900

    feat: POST request 도입

--- categories/views.py
+++ categories/views.py
@@ -5,16 +5,35 @@ from .serializers import CategorySerializer
 
 
 # Create your views here.
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    # single_category = Category.objects.get(pk=1)
-    # serializer = CategorySerializer(single_category)
-    return Response(
-        {
-            "ok": True,
-            # "categories": Category.objects.all(),
-            "categories": serializer.data,
-        }
-    )
+
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        Category.objects.create(
+            name=request.data["name"],
+            kind=request.data["kind"],
+        )
+        return Response({"created": True})
+
+    # Version 1)
+    # all_categories = Category.objects.all()
+    # serializer = CategorySerializer(all_categories, many=True)
+    # # single_category = Category.objects.get(pk=1)
+    # # serializer = CategorySerializer(single_category)
+    # return Response(
+    #     {
+    #         "ok": True,
+    #         # "categories": Category.objects.all(),
+    #         "categories": serializer.data,
+    #     }
+    # )
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
