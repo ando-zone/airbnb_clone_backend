70da1f76466568815e397f266ee15ae79915f60f
ando-zone <dev.dohyeonan@gmail.com>
2023-02-23 18:14:19 +0900

    feat: serializers.py 생성 및 도입

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
+
 
 # Create your views here.
 @api_view()
 def categories(request):
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    # single_category = Category.objects.get(pk=1)
+    # serializer = CategorySerializer(single_category)
     return Response(
         {
             "ok": True,
-            "categories": Category.objects.all(),
+            # "categories": Category.objects.all(),
+            "categories": serializer.data,
         }
     )
