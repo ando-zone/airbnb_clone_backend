4201128997c35ed4d8fbc41d1e50cf8957aaace1
ando-zone <dev.dohyeonan@gmail.com>
2023-02-23 18:40:27 +0900

    feat: category에PUT 메서드 도입

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
@@ -45,8 +46,32 @@ def categories(request):
     #     }
     # )
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        # create할 때와는 다르게 CategorySerializer 인자에 두개가 들어감, partial은 제외하고
+        # category는 원래 것, 그리고 data는 수정할 것.
+        # 따라서 알아서 def update를 실행시키는구나를 인지함. save() 메서드가.
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+    
+    # Version 1)
+    # category = Category.objects.get(pk=pk)
+    # serializer = CategorySerializer(category)
+    # return Response(serializer.data)
