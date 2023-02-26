5e9ec7d55a552bf0cae6ebc74621128a4eb7f883
ando-zone <dev.dohyeonan@gmail.com>
2023-02-26 16:35:26 +0900

    feat: wishlist에 대하여 user가 like 했는지 알아보는 기능 추가

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import SerializerMethodField
 from .models import Amenity, Room
+from wishlists.models import Wishlist
 from users.serializers import TinyUserSerializer
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
@@ -49,6 +50,7 @@ class RoomDetailSerializer(ModelSerializer):
     category = CategorySerializer(read_only=True)
     rating = SerializerMethodField()
     is_owner = SerializerMethodField()
+    is_liked = SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -62,6 +64,13 @@ class RoomDetailSerializer(ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__pk=room.pk,
+        ).exists()
 
     # create나 save 메서드의 self 다음 인자인 validated_data에 추가로 데이터를 추가해주고 싶다면 \
     # 해줘야할 것은 serializer.save()를 호출할 때 데이터를 추가해주는 것이다.
