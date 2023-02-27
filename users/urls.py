f555850dc243b565a0ab47ec9cef4d9d3a54bc47
ando-zone <dev.dohyeonan@gmail.com>
2023-02-27 21:50:45 +0900

    feat: publicuser finding and change_password에 대한 API 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+# 다른 것과 마찬가지로 url check를 진행할 때,
+# 위에서 아래로 순서대로 진행함.
 urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
+    path("change-password", views.ChangePassword.as_view()),
+    path("@<str:username>", views.PublicUser.as_view()),
+
     # path("change-password", views.ChangePassword.as_view()),
     # path("log-in", views.LogIn.as_view()),
     # path("log-out", views.LogOut.as_view()),
