3fae5a40c8697c695e6a492bdc13719ca44c4927
ando-zone <dev.dohyeonan@gmail.com>
2023-03-03 23:02:41 +0900

    feat: /experiences/1 put delete 기능 구현, /experiences/1/perks get 기능 구현

--- experiences/urls.py
+++ experiences/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import PerkDetail, Perks, Experiences, ExperienceDetail
+from .views import (
+    PerkDetail,
+    Perks,
+    Experiences,
+    ExperienceDetail,
+    ExperiencePerks,
+)
 
 
 urlpatterns = [
     path("", Experiences.as_view()),
     path("<int:pk>", ExperienceDetail.as_view()),
+    path("<int:pk>/perks", ExperiencePerks.as_view()),
     path("perks/", Perks.as_view()),
     path("perks/<int:pk>", PerkDetail.as_view()),
+    # path("<int:pk>/reviews", views.RoomReviews.as_view()),
+    # path("<int:pk>/photos", views.RoomPhotos.as_view()),
+    # path("<int:pk>/bookings", views.RoomBookings.as_view()),
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, ParseError
+from rest_framework.exceptions import NotFound, ParseError, PermissionDenied
 from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Perk, Experience
 from .serializers import (
@@ -41,12 +43,13 @@ class Experiences(APIView):
             # Room과는 다른 방식으로 진행했음.
             perk_pks = request.data.get("perks")
             perks = list()
-            for perk_pk in perk_pks:
-                try:
-                    perk = Perk.objects.get(pk=perk_pk)
-                except Exception:
-                    raise ParseError("Perk not found")
-                perks.append(perk)
+            if perk_pks:
+                for perk_pk in perk_pks:
+                    try:
+                        perk = Perk.objects.get(pk=perk_pk)
+                    except Exception:
+                        raise ParseError("Perk not found")
+                    perks.append(perk)
 
             experience = serializer.save(
                 host=request.user,
@@ -75,6 +78,91 @@ class ExperienceDetail(APIView):
             experience, context={"request": request}
         )
 
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        experience = self.get_object(pk)
+
+        if experience.host != request.user:
+            raise PermissionDenied
+
+        serializer = ExperienceDetailSerializer(
+            experience,
+            data=request.data,
+            partial=True,
+            context={"request": request},
+        )
+        if serializer.is_valid():
+            category = None
+            category_pk = request.data.get("category")
+
+            if category_pk:
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.ROOMS:
+                        raise ParseError(
+                            "The category kind should be 'experiences'."
+                        )
+                except Category.DoesNotExist:
+                    raise ParseError("Category not found")
+
+            # Room과는 다른 방식으로 진행했음.
+            perk_pks = request.data.get("perks")
+            perks = list()
+            if perk_pks:
+                for perk_pk in perk_pks:
+                    try:
+                        perk = Perk.objects.get(pk=perk_pk)
+                    except Exception:
+                        raise ParseError("Perk not found")
+                    # TODO@Ando: 기존에 선택된 것에서 빼거나 추가하는 방식은 어떻게 구현하려나?
+                    perks.append(perk)
+
+            experience = serializer.save(
+                host=request.user,
+                category=category,
+                perks=perks,
+            )
+            serializer = ExperienceDetailSerializer(
+                experience, context={"request": request}
+            )
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        experience = self.get_object(pk)
+
+        if experience.host != request.user:
+            raise PermissionDenied
+
+        experience.delete()
+
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+class ExperiencePerks(APIView):
+    def get_object(self, pk):
+        try:
+            return Experience.objects.get(pk=pk)
+        except Experience.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            # TODO@Ando: page가 마지막 페이지를 넘어가면 마지막 페이지를 보여주는 것도 좋을 것 같다.
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = settings.PAGE_SIZE
+        start = (page - 1) * page_size
+        end = start + page_size
+        expereince = self.get_object(pk)
+        serializer = PerkSerializer(
+            expereince.perks.all()[start:end],
+            many=True,
+        )
         return Response(serializer.data)
 
 
