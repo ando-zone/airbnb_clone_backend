da025ead41f30a363eab63636a0c4ef0b3bb703c
ando-zone <dev.dohyeonan@gmail.com>
2023-03-04 21:07:45 +0900

    test: AmenityDetail에 대한 테스트 작성

--- rooms/tests.py
+++ rooms/tests.py
@@ -51,4 +51,47 @@ class TestAmenities(APITestCase):
 
         self.assertEqual(response.status_code, 400)
         self.assertIn("name", data)
+
+class TestAmenity(APITestCase):
+
+    NAME = "Amenity Test"
+    DESC = "Amenity Des"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/1")
+
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+        )
+
+    def test_put_amenity(self):
+        #TODO@Ando: put_amenity test 작성하기
+        pass
+
+    def test_delete_amenity(self):
+
+        response = self.client.delete("/api/v1/rooms/amenities/1")
+
+        self.assertEqual(response.status_code, 204)
         
