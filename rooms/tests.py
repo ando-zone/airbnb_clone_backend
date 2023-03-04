98eaeefecf459367d4b8fa4c2980090ad8a61d92
ando-zone <dev.dohyeonan@gmail.com>
2023-03-04 21:29:04 +0900

    test: Rooms test_code 기초 작성

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,10 +1,11 @@
 # from django.test import TestCase
 from rest_framework.test import APITestCase
 from . import models
+from users.models import User
+
 
 # Create your tests here.
 class TestAmenities(APITestCase):
-
     NAME = "Amenity Test"
     DESC = "Amenity Des"
     URL = "/api/v1/rooms/amenities/"
@@ -41,8 +42,8 @@ class TestAmenities(APITestCase):
         # print(data)
 
         self.assertEqual(response.status_code, 200, "Not 200 status code")
-        self.assertEqual(data["name"],new_amenity_name)
-        self.assertEqual(data["description"],new_amenity_description)
+        self.assertEqual(data["name"], new_amenity_name)
+        self.assertEqual(data["description"], new_amenity_description)
 
         response = self.client.post(self.URL)
         data = response.json()
@@ -52,16 +53,13 @@ class TestAmenities(APITestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn("name", data)
 
-class TestAmenity(APITestCase):
 
+class TestAmenity(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Des"
 
     def setUp(self):
-        models.Amenity.objects.create(
-            name=self.NAME,
-            description=self.DESC
-        )
+        models.Amenity.objects.create(name=self.NAME, description=self.DESC)
 
     def test_amenity_not_found(self):
         response = self.client.get("/api/v1/rooms/amenities/2")
@@ -69,7 +67,6 @@ class TestAmenity(APITestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_get_amenity(self):
-
         response = self.client.get("/api/v1/rooms/amenities/1")
 
         self.assertEqual(response.status_code, 200)
@@ -86,12 +83,31 @@ class TestAmenity(APITestCase):
         )
 
     def test_put_amenity(self):
-        #TODO@Ando: put_amenity test 작성하기
+        # TODO@Ando: put_amenity test 작성하기
         pass
 
     def test_delete_amenity(self):
-
         response = self.client.delete("/api/v1/rooms/amenities/1")
 
         self.assertEqual(response.status_code, 204)
-        
+
+
+class TestRooms(APITestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+    def test_create_room(self):
+        response = self.client.post("/api/v1/rooms/")
+
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(self.user)
+        # self.client.login(username="test",password="123")
+        response = self.client.post("/api/v1/rooms/")
+        self.assertEqual(response.status_code, 400)
+        # print(response.json())
